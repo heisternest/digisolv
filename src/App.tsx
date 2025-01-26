7a06f3f6135ffe18729815fb1f12bf0bd0de595a
heisternest <heisternest.com@gmail.com>
2025-01-26 13:41:45 +0545

    remove bento

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { GlobeDemo } from "./components/sections/animated-hero";
 import { ContactPage } from "./components/sections/contact";
 import FooterSection from "./components/sections/footer";
 import { Hero } from "./components/sections/hero";
-import { BentoDemo } from "./components/sections/projects";
 import { AnimatedTestimonialsDemo } from "./components/sections/testimonials";
 
 function App() {
@@ -23,13 +22,22 @@ function App() {
         <About />
       </div>
 
-      <div className="mt-32 px-0 lg:px-32 md:px-32 ">
-        <BentoDemo />
-      </div>
-
       <div className="mt-30">
         <AppleCardsCarouselDemo />
       </div>
+      {/* <div className="mt-32 px-0 lg:px-54 md:px-32 ">
+        <div className="my-10">
+          <h1 className="text-center text-4xl font-bold text-gray-800 ">
+            Our Projects
+          </h1>
+          <p className="text-center text-lg text-gray-600">
+            We have worked on a wide range of projects. Here are some of our
+            projects.
+          </p>
+        </div>
+        <BentoDemo />
+      </div> */}
+
       <div className="mt-5">
         <LogoCarouselDemo />
       </div>
--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import * as React from "react";
 import { DayPicker } from "react-day-picker";
 
@@ -55,6 +56,14 @@ function Calendar({
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
         ...classNames,
+      }}
+      components={{
+        IconLeft: ({ className, ...props }) => (
+          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+        ),
+        IconRight: ({ className, ...props }) => (
+          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+        ),
       }}
       {...props}
     />
